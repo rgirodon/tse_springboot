139e85718c10dbba210dad6cac2c97ae2e2c39bf
rgirodon <remy.girodon@gmail.com>
2020-10-27 15:23:44 +0100

    Remove unuseful component scanning activation

--- src/main/java/org/rygn/tse_springboot/config/ApplicationConfig.java
+++ src/main/java/org/rygn/tse_springboot/config/ApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "org.rygn.tse_springboot")
 public class ApplicationConfig {
 	
 	@Bean

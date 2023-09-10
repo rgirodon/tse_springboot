ad22fc4af775ce2a6954ad17d40e48a16f1917d4
rgirodon <remy.girodon@gmail.com>
2023-09-10 10:57:48 +0200

    Upgrade SpringBoot

---
+++ src/main/java/org/rygn/tse_springboot/components/Parrot.java
@@ -0,0 +1,11 @@
+package org.rygn.tse_springboot.components;
+
+import org.rygn.tse_springboot.interfaces.Bird;
+
+public class Parrot implements Bird {
+
+	@Override
+	public String toString() {
+		return "Parrot instance";
+	}
+}
--- src/main/java/org/rygn/tse_springboot/config/ApplicationConfig.java
+++ src/main/java/org/rygn/tse_springboot/config/ApplicationConfig.java
@@ -1,15 +1,16 @@
 package org.rygn.tse_springboot.config;
 
 import org.rygn.tse_springboot.components.Owl;
+import org.rygn.tse_springboot.components.Parrot;
+import org.rygn.tse_springboot.interfaces.Bird;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
 	
 	@Bean
-	public Owl owl() {
+	public Bird bird() {
 		
 		return new Owl();
 	}
